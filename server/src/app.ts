92b76f422f67fc687ef09db78d459b5266f8a527
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-07 16:09:28 +0530

    create middleware for express session and passport

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,8 @@
 import  express,{Request,Response} from "express";
+import passport from "passport";
+import session from "express-session";
 import cookieParser from "cookie-parser";
+import { Strategy } from "passport-google-oauth20";
 import cors from 'cors'
 import config from "./config/config";
 import { authRouter } from "./routes/auth.routes";
@@ -14,6 +17,14 @@ app.use(cors({
     origin:config.Client_Route,
     credentials:true
 }))
+app.use(session({
+    secret:config.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 app.get("/",(req:Request,res:Response)=>{
     res.send("server is runnig")
