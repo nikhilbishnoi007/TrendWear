6a8aead03f9786d01ab7c7102178e126b1e2e04a
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-15 16:36:31 +0530

    creat route for login and logout

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import * as authController from "../controllers/auth.controllers"
-export  const authRouter=Router()
+const authRouter=Router()
 
 authRouter.post("/register",authController.register)
+authRouter.post("login",authController.login)
+authRouter.get("/logout",authController.logout)
 
+
+export default authRouter
 
