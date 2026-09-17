4faf8b1a2322094afa145b0ab804d2966054c544
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-17 11:11:34 +0530

    fix a server listening issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,9 @@ import authRouter  from "./routes/auth.routes";
 import adminRouter from "./routes/admin.routes";
 
 
-const app=express()
+
+ const app=express()
+
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true}))
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import app from "./app";
+import app  from "./app";
 import config from "./config/config";
 import { connectDb } from "./config/db";
 
